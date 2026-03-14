7a4962aa7d2d7836f26988b9ed5147299837d55d
jacka10peil123@gmail.com <146150116+bixiphel@users.noreply.github.com>
2026-03-14 16:21:06 -0500

    Renamed parameters and implemented complex exp()

--- src/complex.c
+++ src/complex.c
@@ -1,16 +1,22 @@
 #include "complex.h"
+#include <math.h>
 
-Complex c_add(Complex a, Complex b) {
-	Complex z = {a.real + b.real, a.imag + b.imag};
+Complex c_add(Complex z1, Complex z2) {
+	Complex z = {z1.real + z2.real, z1.imag + z2.imag};
 	return z;	
 }
 
-Complex c_sub(Complex a, Complex b) {
-	Complex z = {a.real - b.real, a.imag - b.imag};
+Complex c_sub(Complex z1, Complex z2) {
+	Complex z = {z1.real - z2.real, z1.imag - z2.imag};
 	return z;	
 }
 
-Complex c_mul(Complex a, Complex b) {
-	Complex z = {a.real*b.real - a.imag*b.imag, a.real*b.imag + a.imag*b.real}; 
+Complex c_mul(Complex z1, Complex z2) {
+	Complex z = {z1.real*z2.real - z1.imag*z2.imag, z1.real*z2.imag + z1.imag*z2.real}; 
 	return z;	
+}
+
+Complex c_exp(Complex z1) {
+	Complex z = {exp(z1.real)*cos(z1.imag), exp(z1.real)*sin(z1.imag)};
+	return z;
 }
--- src/complex.h
+++ src/complex.h
@@ -6,8 +6,9 @@ typedef struct {
 	double imag;
 } Complex;
 
-Complex c_add(Complex a, Complex b);
-Complex c_sub(Complex a, Complex b);
-Complex c_mul(Complex a, Complex b);
+Complex c_add(Complex z1, Complex z2);
+Complex c_sub(Complex z1, Complex z2);
+Complex c_mul(Complex z1, Complex z2);
+Complex c_exp(Complex z1);
 
 #endif
