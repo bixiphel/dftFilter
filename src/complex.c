6166f190cc5c4d582cb56c16e6699e7f1bc2196e
jacka10peil123@gmail.com <146150116+bixiphel@users.noreply.github.com>
2026-03-14 15:59:41 -0500

    Implemented subtraction.

--- src/complex.c
+++ src/complex.c
@@ -5,6 +5,10 @@ Complex c_add(Complex a, Complex b) {
 	return z;	
 }
 
+Complex c_sub(Complex a, Complex b) {
+	Complex z = {a.real - b.real, a.imag - b.imag};
+	return z;	
+}
 
 Complex c_mul(Complex a, Complex b) {
 	Complex z = {a.real*b.real - a.imag*b.imag, a.real*b.imag + a.imag*b.real}; 
--- src/complex.h
+++ src/complex.h
@@ -7,6 +7,7 @@ typedef struct {
 } Complex;
 
 Complex c_add(Complex a, Complex b);
+Complex c_sub(Complex a, Complex b);
 Complex c_mul(Complex a, Complex b);
 
 #endif
