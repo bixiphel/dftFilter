9cb67f5e0fdb6a21f74f517fc2953b2a036aea22
jacka10peil123@gmail.com <146150116+bixiphel@users.noreply.github.com>
2026-03-15 15:51:18 -0500

    Implemented writing to a PGM and fixed bugs with reading a file in.

--- src/pgm.c
+++ src/pgm.c
@@ -14,7 +14,8 @@ void readPGM(const char *filename, PGMimage *img) {
 
 
 	// Helper variables to parse file
-	int c;			// Stores PGM file format (2 or 5 depending on the file)
+	int format;		// Stores PGM file format (2 or 5 depending on the file)
+	int c;			// Helps with skipping comment line(s) 
 	char buf[1024];		// Buffer used to skips any comment lines in hte file 
 	
 	// Skips comment lines in the file
@@ -25,13 +26,13 @@ void readPGM(const char *filename, PGMimage *img) {
 
 
 	// Get the magic nubmer for the PGM file ("Px", where 'x' is 2 or 5)
-	if (fscanf(fp, "P%d\n", &c) != 1) {
+	if (fscanf(fp, "P%d\n", &format) != 1) {
 		printf ("read error ....");
 		fclose(fp);
        		exit(11);
 	}
 	// Validates the magic number is 2 or 5 (the only formats allowed by the PGM specification)
-	if (c != 5 && c != 2) {
+	if (format != 5 && format != 2) {
 		printf ("read error ....");
 		fclose(fp);
 		exit(12);
@@ -40,7 +41,7 @@ void readPGM(const char *filename, PGMimage *img) {
 
 	// Skips any more comment lines left in the file
 	while ((c=fgetc(fp)) == '#')
-		fgets(buf, 1024, fp);
+		fgets(buf, sizeof(buf), fp);
 	ungetc(c, fp);
 
 	// Attempts to read the image dimensions 
@@ -53,19 +54,18 @@ void readPGM(const char *filename, PGMimage *img) {
 	
 	// Allocates memory for the image if possible	
 	img->data = malloc(img->width*img->height);
-	getc(fp);
 	if(!img->data) {
 		printf("Memory allocation failed\n");
 		fclose(fp);
 		exit(14);
 	}
-		
+	fgetc(fp);
 
 	// Processes the file according to the magic number
-	if (c==5) {
+	if (format == 5) {
 		// Under PGM specification, 5 corresponds to binary values
 		fread(img->data, 1, img->width * img->height, fp);
-	} else if(c == 2) {
+	} else if(format == 2) {
 		// 2 corresponds to ASCII values
 		int val;
 
@@ -79,4 +79,25 @@ void readPGM(const char *filename, PGMimage *img) {
 	fclose(fp);
 }
 
-void writePGM(const char *filename, const PGMimage *img);
+
+
+void writePGM(const char *filename, const PGMimage *img) {
+	// Attempt to open file in read-binary mode
+	FILE *fp = fopen(filename, "wb");
+
+	// Verify the file opened; exits program if file cannot be opened.
+	if(!fp) {
+		printf("Error opening write file.\n");
+		exit(15);
+	}
+
+	// Write the file header as per PGM specs
+	// **Note: this uses the P5 (binary) format
+	fprintf(fp, "P5\n%d %d\n%d\n", img->width, img->height, img->max);
+
+	// Write the pixel data
+	fwrite(img->data, 1, img->width * img->height, fp);
+
+	// Close the file
+	fclose(fp);
+}
