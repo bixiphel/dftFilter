24b9ed8a158f78e0e18b5e87fd0e53a4255e30ca
jacka10peil123@gmail.com <146150116+bixiphel@users.noreply.github.com>
2026-03-17 19:57:38 -0500

    Bunch of bug fixes.

--- src/2dDFT.c
+++ src/2dDFT.c
@@ -16,67 +16,81 @@ int main() {
 	resized.height = 2*img.height - 1;
 	resized.max = img.max;
 	unsigned char *rData = malloc(resized.width * resized.height);
-	
+
+	// Zero the rData array
+	for(int i = 0; i < resized.width*resized.height; i++) {
+		rData[i] = 0;
+	}
+
+	// Put the original image data in the top-left corner and "center" it
 	for (int y = 0; y < img.height; y++) {
         	for (int x = 0; x < img.width; x++) {
-            		rData[y * resized.width + x] = img.data[y * img.width + x];
+            		rData[y * resized.width + x] = img.data[y * img.width + x] * pow(-1, x + y);
         	}
     	}
 	resized.data = rData;
 	
-	Complex temp[img.height][img.width];
+	// Stores the image data as complex numbers
+	Complex *temp = malloc(sizeof(Complex)*resized.width*resized.height);
 
 	// Perform the DFT on all rows 
-	for(int y = 0; y < img.height; y++) {
-		for(int k = 0; k < img.width; k++) {
+	for(int y = 0; y < resized.height; y++) {
+		for(int u = 0; u < resized.width; u++) {
 			Complex num = {0.0f, 0.0f};
 
-			for(int x = 0; x < img.width; x++) {
-				double exponent = -2.0f*PI*k*x/(img.width);
+			for(int x = 0; x < resized.width; x++) {
+				double exponent = -2.0f*PI*u*x/(resized.width);
 				Complex i = {0, 1};
 				Complex power = c_mul(i, c_rep(exponent));
 				Complex e = c_exp(power);
-				Complex transform = c_mul(e, c_rep(img.data[y*img.width + x]));
+				Complex transform = c_mul(e, c_rep((double)resized.data[y*resized.width + x]));
 				num = c_add(num, transform);
 			}
 
-			temp[y][k] = num;
+			temp[y*resized.width + u] = num;
 		}
 	}
 
 	// Perform the DFT on all columns
-	double output[img.height][img.width];
+	double *output = malloc(sizeof(double)*resized.width*resized.height);
 	double max = 0.0f;
 
-	for(int x = 0; x < img.width; x++) {
-		for(int v = 0; v < img.height; v++) {
+	for(int x = 0; x < resized.width; x++) {
+		for(int v = 0; v < resized.height; v++) {
 			Complex num = {0.0f, 0.0f};
 
-			for(int y = 0; y < img.height; y++) {
-				double exponent = -2.0f*PI*v*y/(img.width);
+			for(int y = 0; y < resized.height; y++) {
+				double exponent = -2.0f*PI*v*y/(resized.height);
 				Complex i = {0, 1};
 				Complex power = c_mul(i, c_rep(exponent));
 				Complex e = c_exp(power);
 				
-				num = c_add(num, c_mul(temp[y][x], e));
+				num = c_add(num, c_mul(temp[y*resized.width + x], e));
 			}
-			output[v][x] = c_mag(num);
-			if(output[v][x] >= max) {
-				max = output[v][x];
+			output[v*resized.width + x] = c_mag(num);
+			
+			if(output[v*resized.width + x] >= max) {
+				max = output[v*resized.width + x];
 			}
 		}
 	}
 
 	// Converts the resulting 2D DFT back into unsigned chars 
-	for(int y = 0; y < img.height; y++) {
-		for(int x = 0; x < img.width; x++) {
+	for(int y = 0; y < resized.height; y++) {
+		for(int x = 0; x < resized.width; x++) {
 			// Scales values within the image max using log scaling
-			output[y][x] = log(1 + output[y][x]);
-			rData[y*img.width + x] = (unsigned char) (output[y][x] / log(1 + max) * img.max);
+			output[y*resized.width + x] = log(1 + output[y*resized.width + x]);
+			rData[y*resized.width + x] = (unsigned char) (output[y*resized.width + x] / log(1 + max) * resized.max);
 		}
 	}	
 
+	// Write to the output
 	writePGM("output.pgm", &resized);	
+
+	// Free memory and close resources
+	free(rData);
+	free(temp);
+	free(output);
 
 	return 0;
 }
